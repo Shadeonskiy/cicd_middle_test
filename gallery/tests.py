5e7dbca3b5b87ea09052e31b1b201ae194d8bea4
Shadeonskiy <shadeonskiy@gmail.com>
2023-05-23 13:07:39 +0300

    Added tests for gallery_view

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Image, Category
 
 # Create your tests here.
+class TestGallery(TestCase):
+    def test_gallery_view_success(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_gallery_view_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'gallery.html')
+
+    def test_gallery_view_categories(self):
+        category_1 = Category.objects.create(name="category 1")
+        category_2 = Category.objects.create(name="category 2")
+        category_3 = Category.objects.create(name="category 3")
+
+        response = self.client.get('/')
+
+        categories = response.context['categories']
+        assert len(categories) == 3
+        assert set(categories) == {category_1, category_2, category_3}
